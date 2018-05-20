016118c936c01ff19e40b365b7fe852d99f87792
Jonathan Mai <h.jmai2791@gmail.com>
2018-05-20 16:11:18 -0700

    Finish with logic, only front end prettying up now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,11 @@
 var questionArray = [];
+var answerArray = [];
+var userAnswer = [];
+var timer;
+var count = 10;
+var correct = wrong = 0;
 // Function to create question objects
-function Question(question, a, b, c, d, answer){
+function Question(question, a, b, c, d, answer) {
     var form = new Object();
     form.question = question;
     form.a = a;
@@ -8,6 +13,7 @@ function Question(question, a, b, c, d, answer){
     form.c = c;
     form.d = d;
     form.answer = answer;
+    answerArray.push(form.answer);
     questionArray.push(form);
 }
 
@@ -51,25 +57,64 @@ Question(
     'Guilford Courthouse, North Carolina',
 )
 
-function populateQuestion(question){
+function populateQuestion(question) {
     $('.question').empty();
-    $('.question').html(
-        '<h4>'+question.question+'</h4>'+
-        '<p data-answerA="'+question.a+'">'+question.a+'</p>'+
-        '<p data-answerB="'+question.b+'">'+question.b+'</p>'+
-        '<p data-answerC="'+question.c+'">'+question.c+'</p>'+
-        '<p data-answerD="'+question.d+'">'+question.d+'</p>'
-    )
-}
-
-for(var i = 0; i < questionArray.length; i++){
-    var counter = 10;
-    var timer = setInterval(function(){
-        $('#timer').text('Time remaining: '+counter);
-        if (counter === 0) {
-            $('#timer').text('Times up!');
-            clearInterval(timer);
+    if(questionArray.length === 0){
+        $('#timer').text('Game is done!');
+        for(var j = 0; j < answerArray.length; j++){
+            if(answerArray[j] === userAnswer[j]){
+                console.log('Correct: '+answerArray[j]+' and '+userAnswer[j]);
+                correct++;
+            }
+            else{
+                console.log('Wrong: '+answerArray[j]+' and '+userAnswer[j]);
+                wrong++;
+            }
         }
-        counter--;
+        $('.question').html(
+            '<p>Correct Answers: '+correct+'</p>'+
+            '<p>Incorrect Answers: '+wrong+'</p>'
+        )
+    }
+    else{
+        $('.question').html(
+            '<h4>' + question.question + '</h4>' +
+            '<p class="choice" data-answer="' + question.a + '">' + question.a + '</p>' +
+            '<p class="choice" data-answer="' + question.b + '">' + question.b + '</p>' +
+            '<p class="choice" data-answer="' + question.c + '">' + question.c + '</p>' +
+            '<p class="choice" data-answer="' + question.d + '">' + question.d + '</p>'
+        )
+        $('.choice').on('click', function(){
+            userAnswer.push($(this).data('answer'));
+            stopTimer();
+            count = 10;
+            questionArray.shift();
+            populateQuestion(questionArray[0]);
+        });
+        startTimer();
+    }
+}
+function startTimer() {
+    timer = setInterval(function(){
+        $('#timer').text('Time Remaining: '+count);
+        if(count === 0){
+            stopTimer();
+            count = 10;
+            userAnswer.push("");
+            questionArray.shift();
+            $('#timer').text('Times up! Next question!');
+            setTimeout(populateQuestion(questionArray[0]), 3000);
+        }
+        count--;
     }, 1000);
 }
+function stopTimer(){
+    clearInterval(timer);
+}
+
+$('.start').on('click', startTimer);
+$('.stop').on('click', stopTimer);
+$('.clear').on('click', function(){
+    count = 10;
+});
+populateQuestion(questionArray[0]);
